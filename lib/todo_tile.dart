47a276cd48580fbb711a09060b52412615a16ffb
khanindrare <khanindrare@gmail.com>
2024-07-05 17:01:45 +0000

    V5

--- lib/todo_tile.dart
+++ lib/todo_tile.dart
@@ -1,33 +1,389 @@
 import 'package:flutter/material.dart';
 
-
-
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  const ToDoTile({Key? key}) : super(key: key);
 
   @override
-@override
-Widget build(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.all(25.0),
-    child: Container(
-      padding: EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.circular(12),
-      ),
-child: Column(
-        children: [
-          Text("Learn Flutter!"),
-          Text("Learn Languages."),
-          
-        ],
-      ),
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Box 1
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Learn Flutter"),
+                  SizedBox(height: 8),
+                  Text("Partly done."),
+                ],
+              ),
+            ),
 
+            // Box 2
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Learn React"),
+                  SizedBox(height: 8),
+                  Text("Working on it... going good"),
+                ],
+              ),
+            ),
+
+            // Box 3
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Learn more cross languages for cross platforms"),
+                  SizedBox(height: 8),
+                  Text("Working on it.."),
+                ],
+              ),
+            ),
+
+            // Box 4
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 4"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 4"),
+                ],
+              ),
+            ),
+
+            // Box 5
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 5"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 5"),
+                ],
+              ),
+            ),
+
+            // Box 6
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 6"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 6"),
+                ],
+              ),
+            ),
+
+            // Box 7
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 7"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 7"),
+                ],
+              ),
+            ),
+
+            // Box 8
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.indigo,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 8"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 8"),
+                ],
+              ),
+            ),
+
+            // Box 9
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 9"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 9"),
+                ],
+              ),
+            ),
+
+            // Box 10
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.lightGreen,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 10"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 10"),
+                ],
+              ),
+            ),
+
+            // Box 11
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 11"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 11"),
+                ],
+              ),
+            ),
+
+            // Box 12
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 12"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 12"),
+                ],
+              ),
+            ),
+
+            // Box 13
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.cyan,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 13"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 13"),
+                ],
+              ),
+            ),
+
+            // Box 14
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 14"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 14"),
+                ],
+              ),
+            ),
+
+            // Box 15
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.lime,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 15"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 15"),
+                ],
+              ),
+            ),
+
+            // Box 16
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.brown,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 16"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 16"),
+                ],
+              ),
+            ),
+
+            // Box 17
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 17"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 17"),
+                ],
+              ),
+            ),
+
+            // Box 18
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.deepOrangeAccent,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 18"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 18"),
+                ],
+              ),
+            ),
+
+            // Box 19
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.tealAccent,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 19"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 19"),
+                ],
+              ),
+            ),
+
+            // Box 20
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Box 20"),
+                  SizedBox(height: 8),
+                  Text("Content for Box 20"),
+                ],
+              ),
+            ),
+          ],
         ),
-  
+      ),
     );
-
+  }
 }
-}
 
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('To Do List'),
+      ),
+      body: ToDoTile(),
+    ),
+  ));
+}
